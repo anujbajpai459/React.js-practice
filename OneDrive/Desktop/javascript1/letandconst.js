3af31d4e7b0fe0c91a1c6ebe3ba259821a6e2a73
anujbajpai459 <anujb9152@gmail.com>
2026-05-09 00:45:47 +0530

    day-1 javascript

---
+++ OneDrive/Desktop/javascript1/letandconst.js
@@ -0,0 +1,15 @@
+//var - old way of creating variables. 
+//problem with var - it can be re-declared and updated 
+var name="anuj";
+console.log(name);
+
+//let - Modern wayand used when value can change.
+let age = 22;
+age = 23;
+console.log(age);
+
+// const - used when value should Not change
+ const pi=3.14;
+ console.log(pi);
+ 
+//notes-var is old and function-scoped, let allows value updates, and const creates fixed values that cannot be changed.
